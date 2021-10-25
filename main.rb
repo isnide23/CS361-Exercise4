02e1c3dc70359ebf9095929b35debc47e6af92cc
Ian Snyder <isnide23@gmail.com>
2021-10-25 11:17:50 -0700

    wrap candy machine

--- main.rb
+++ main.rb
@@ -10,66 +10,77 @@ class MentalState
     def do_work
       # Amazing stuff...
     end
+end
+  
+def audit_sanity(bedtime_mental_state)
+  if bedtime_mental_state.audit!.ok?
+    MorningMentalState.new(:ok)
+  else 
+    MorningMentalState.new(:not_ok)
   end
-  
-  def audit_sanity(bedtime_mental_state)
-    return 0 unless bedtime_mental_state.auditable?
-    if bedtime_mental_state.audit!.ok?
-      MorningMentalState.new(:ok)
-    else 
-      MorningMentalState.new(:not_ok)
-    end
-  end
-  
-  if audit_sanity(bedtime_mental_state) == 0
-    puts "error"
-  else
-    new_state = audit_sanity(bedtime_mental_state)
-  end
-  
-  
-  
-  
-  
-  # Exercise 5 Part 2 (Don't Return Null / Null Object Pattern)
-  
-  class BedtimeMentalState < MentalState ; end
-  
-  class MorningMentalState < MentalState ; end
-  
-  def audit_sanity(bedtime_mental_state)
-    return nil unless bedtime_mental_state.auditable?
-    if bedtime_mental_state.audit!.ok?
-      MorningMentalState.new(:ok)
-    else 
-      MorningMentalState.new(:not_ok)
-    end
-  end
-  
+end
+
+if audit_sanity(bedtime_mental_state) == 0
+  puts "error"
+else
   new_state = audit_sanity(bedtime_mental_state)
-  new_state.do_work
-  
-  
-  
-  
-  # Exercise 5 Part 3 (Wrapping APIs)
-  
-  require 'candy_service'
-  
-  class CandyMachine
+end
 
-    def initialize
-        
-    end
 
+
+
+
+# Exercise 5 Part 2 (Don't Return Null / Null Object Pattern)
+
+class BedtimeMentalState < MentalState ; end
+
+class MorningMentalState < MentalState ; end
+
+def audit_sanity(bedtime_mental_state)
+  return nil unless bedtime_mental_state.auditable?
+  if bedtime_mental_state.audit!.ok?
+    MorningMentalState.new(:ok)
+  else 
+    MorningMentalState.new(:not_ok)
+  end
+end
+
+new_state = audit_sanity(bedtime_mental_state)
+new_state.do_work
+
+
+
+
+# Exercise 5 Part 3 (Wrapping APIs)
+
+require 'candy_service'
+
+class MyCandyMachine
+
+  def initialize(candy_machine)
+    @candy_machine = candy_machine
+  end
+
+  def prepare
 
   end
-  
-  machine = CandyMachine.new
-  machine.prepare
-  
-  if machine.ready?
-    machine.make!
-  else
-    puts "sadness"
+
+  def ready?
+
   end
+
+  def make!
+
+  end
+
+end
+
+machine = CandyMachine.new
+my_candy_machine = MyCandyMachine.new(machine)
+machine.prepare
+
+if machine.ready?
+  machine.make!
+else
+  puts "sadness"
+end
